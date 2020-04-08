9c8bf4530cae9180d73a3aacbaba7de693ccda26
Guillaume Lemiere <guillaumelemiere@MacBook-Pro-de-Guillaume.local>
2020-04-08 18:15:19 +0200

    modification Create en TextInput

--- src/Frontend/App.js
+++ src/Frontend/App.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import Create from './components/Create'
+import { TextInput } from 'react-native-gesture-handler';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Create/>
+      <TextInput/>
     );
   }
 }
